125f5ae093044eb8bc0be1f299b7957a5d87b194
Viktor Okhotnikov <viktorokh96@mail.ru>
2016-12-03 03:35:19 +0300

    незначительные поправки

--- vomit/src/game/game.cpp
+++ vomit/src/game/game.cpp
@@ -1,17 +1,19 @@
 #include <vomitgame.hpp>
+#include <new>
 
 enum builderName { game, menu, rating, setting };
 
 int VomitGame::init()
 {
-	_builder[game] = new GameBuilder;
-	_builder[menu] = new MenuBuilder;
-	_builder[rating] = new RatingBuilder;
-	_builder[setting] = new SettingBuilder;
+	try {
+		_builder[game] = new GameBuilder;
+		_builder[menu] = new MenuBuilder;
+		_builder[rating] = new RatingBuilder;
+		_builder[setting] = new SettingBuilder;
 
-	for (int i = 0; i < _buildQuantity; ++i)
-		if ( ! _builder[i] )
-			return -1;
+	} catch (std::bad_alloc bad) {
+		return -1;
+	}
 	
 	return 0;
 }
