71e411b02a6333f7d62303e9e77b180438dc0443
Viktor Okhotnikov <viktorokh96@mail.ru>
2016-12-03 03:27:38 +0300

    Уточнены модели
    Расширены и уточнены модели игры

--- vomit/include/model/gamemodel.hpp
+++ vomit/include/model/gamemodel.hpp
@@ -1,16 +1,45 @@
 #ifndef _GAME_MODEL
 #define _GAME_MODEL 1
 
-typedef int speed_t;
+#include "vomitmodel.hpp"
 
-class GameModelFacade {
-};
+typedef int speed_t;
+typedef int coord_t;
+typedef int cmd_t;
+typedef int event_t;
 
 class MapModel : public AbstractModel {
 	speed_t speed;
+public:
+	void update();
+};
+
+class ObstacleModel : public AbstractModel {
+	coord_t coord;
+	speed_t speed;
+public:
+	void update();
 };
 
 class TadpoleModel : public AbstractModel {
+	MapModel * const _map;
+public:
+	TadpoleModel( MapModel *map );
+	void update();
+};
+
+/*
+ * Шаблон проетирования "Фасад"
+ * реализует унифицированный интерфейс к подсистеме
+ * модели игрового мира.
+*/
+class GameModelFacade {
+	MapModel * const _map;
+	TadpoleModel * const _tadpole;
+public:
+	GameModelFacade( MapModel *map, TadpoleModel *tadpole );
+	
+	cmd_t update ( event_t &ev );
 };
 
 #endif
